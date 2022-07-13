eeba88e79d6349792e8d15fab8ffc918ab6e70c1
mark-pooh <mpuahpooh08@gmail.com>
2022-07-13 20:21:57 +0800

    change to accepting input from params

--- UpdateTableFromExcel/Program.cs
+++ UpdateTableFromExcel/Program.cs
@@ -1,7 +1,9 @@
 ﻿using OfficeOpenXml;
-using Oracle.ManagedDataAccess.Client;
 using System;
 using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections.Generic;
 
 namespace UpdateTableFromExcel
 {
@@ -9,7 +11,8 @@ namespace UpdateTableFromExcel
     {
         static void Main(string[] args)
         {
-            string filename = "all range.xlsx";   //args[0];
+            string filename = args[0];
+            string connString = args[1];
 
             try
             {
@@ -17,27 +20,120 @@ namespace UpdateTableFromExcel
 
                 using (var package = new ExcelPackage(new FileInfo(filename)))
                 {
-                    var workSheet = package.Workbook.Worksheets[0];
-                    var start = workSheet.Dimension.Start;
-                    var end = workSheet.Dimension.End;
+                    int count = package.Workbook.Worksheets.Count;
+                    Console.WriteLine("Number of sheets : " + count);
+                    Console.Write("Please insert desired sheet (1 - n): ");
+                    int selection = int.Parse(Console.ReadLine());
 
-                    OracleConnection conn = new OracleConnection("Provider = OraOLEDB.Oracle; Data Source = NEPSDEVL; User Id = NEPSBI; Password = xs2nepsbi");
-                    conn.Open();
-
-                    for (int row = start.Row; row <= end.Row; row++)
+                    if(selection <= count)
                     {
-                        //Row by row
-                        for (int col = start.Column; col <= end.Column; col++)
-                        {
-                            //Cell by cell
-                            object cellValue = workSheet.Cells[row, col].Text;
-                            Console.Write("column : {0}, value: {1}", col, cellValue);
-                        }
-                        Console.WriteLine();
-                    }
+                        var workSheet = package.Workbook.Worksheets[selection - 1];
+                        var start = workSheet.Dimension.Start;
+                        var end = workSheet.Dimension.End;
 
-                    conn.Dispose();
-                    conn.Close();
+                        OleDbConnection conn = new OleDbConnection(connString);
+                        conn.Open();
+
+                        Console.WriteLine("Connection Opened\n");
+                        Console.Write("Please insert table name : ");
+                        string tableName = Console.ReadLine();
+
+                        Console.WriteLine("\nYou've picked table {0}.\n", tableName);
+
+                        OleDbCommand cmd_Segment = new OleDbCommand
+                        {
+                            Connection = conn,
+                            CommandText = "SELECT COLUMN_NAME FROM USER_TAB_COLUMNS WHERE TABLE_NAME = UPPER('" + tableName + "')"
+                        };
+                        cmd_Segment.CommandType = CommandType.Text;
+                        OleDbDataReader dr_Segment = cmd_Segment.ExecuteReader();
+
+                        string colFilter = "", colValue = "";
+
+                        if(dr_Segment != null)
+                        {
+                            int i = 0;
+
+                            List<string> columns = new List<string>();
+
+                            while(dr_Segment.Read())
+                            {
+                                columns.Add(dr_Segment.GetString(0));
+                                Console.WriteLine("{0}) {1}", ++i, dr_Segment.GetString(0));
+                            }
+
+                            Console.Write("\nPlease insert column index to filter (1, 2, n) : ");
+                            selection = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("\nYou've picked column {0} as the filter.", columns[selection - 1]);
+                            colFilter = columns[selection - 1];
+
+                            Console.Write("\nPlease insert column index to change (1, 2, n) : ");
+                            selection = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("\nYou've picked column {0} to be changed.", columns[selection - 1]);
+                            colValue = columns[selection - 1];
+                        }
+
+                        dr_Segment.Close();
+                        cmd_Segment.Dispose();
+
+                        int excelColFilter = 1, excelColValue = 2;
+
+                        Console.WriteLine("\n------------EXCEL------------\n");
+                        Console.WriteLine("Total number of columns = {0}", end.Column);
+
+                        Console.Write("\nPlease insert column index to filter (1, 2, n) : ");
+                        excelColFilter = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\nYou've picked column {0} as the filter.", excelColFilter);
+
+                        Console.Write("\nPlease insert column index to replace (1, 2, n) : ");
+                        excelColValue = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\nYou've picked column {0} as the replacement value.", excelColValue);
+
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+
+                        for (int row = start.Row; row <= end.Row; row++)
+                        {
+                            ////Row by row
+                            //for (int col = start.Column; col <= end.Column; col++)
+                            //{
+                            //    //Cell by cell
+                            //    object cellValue = workSheet.Cells[row, col].Text;
+                            //    Console.Write("column : {0}, value: {1}", col, cellValue);
+                            //}
+                            //Console.WriteLine();
+
+                            string filterVal = "", replaceVal = "";
+
+                            filterVal = workSheet.Cells[row, excelColFilter].Text;
+                            replaceVal = workSheet.Cells[row, excelColValue].Text;
+
+                            Console.WriteLine("Processing row {0}", row);
+
+                            OleDbCommand cmd_UpdateCoor = new OleDbCommand
+                            {
+                                Connection = conn,
+                                CommandText = "UPDATE " + tableName + " SET " + colValue + " = :replace WHERE " + colFilter + " = :filter"
+                            };
+                            cmd_UpdateCoor.Parameters.AddWithValue(":replace", replaceVal);
+                            cmd_UpdateCoor.Parameters.AddWithValue(":filter", filterVal);
+                            cmd_UpdateCoor.ExecuteNonQuery();
+                            cmd_UpdateCoor.Dispose();
+
+                            Console.Clear();
+                        }
+
+                        conn.Dispose();
+                        conn.Close();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid selection");
+                    }
                 }
             }
             catch (IOException e)
@@ -45,6 +141,8 @@ namespace UpdateTableFromExcel
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
+
+            Console.ReadKey();
         }
     }
 }
